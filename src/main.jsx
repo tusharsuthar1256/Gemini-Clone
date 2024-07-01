c05c1984c84ff4ae154d133c45266f3d34c9ef0c
tusharsuthar1256 <153266231+tusharsuthar1256@users.noreply.github.com>
2024-07-01 20:28:44 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import ContextProvider from "./Context/context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+
     <ContextProvider>
       <App />
     </ContextProvider>
