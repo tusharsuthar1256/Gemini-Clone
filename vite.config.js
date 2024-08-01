9ad5c9eafe8f4994be81fea2a7f26bae365d5130
tusharsuthar1256 <153266231+tusharsuthar1256@users.noreply.github.com>
2024-08-01 17:25:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/repository-name/',  // Replace with your repository name
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
 });
